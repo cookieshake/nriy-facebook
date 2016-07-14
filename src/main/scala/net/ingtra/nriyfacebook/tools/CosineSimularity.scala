9945318cb40947ee88d35506a4bb570147ac6f97
cookieshake <nriyays@gmail.com>
2016-07-14 17:15:11 +0900

    Fix mistyped

--- src/main/scala/net/ingtra/nriyfacebook/tools/CosineSimularity.scala
+++ src/main/scala/net/ingtra/nriyfacebook/tools/CosineSimularity.scala
@@ -5,18 +5,18 @@ import scala.math._
 //Python code from http://secmem.tistory.com/670
 
 object CosineSimularity {
-  def apply(map1: Map[Any, Double], map2: Map[Any, Double]): Double = {
+  def apply(map1: Map[String, Double], map2: Map[String, Double]): Double = {
     var maginitudeA: Double = 0
-    map1.foreach((tuple: (Any, Double)) => maginitudeA += pow(tuple._2, 2))
+    map1.foreach((tuple: (String, Double)) => maginitudeA += pow(tuple._2, 2))
 
     var maginitudeB: Double = 0
-    map2.foreach((tuple: (Any, Double)) => maginitudeB += pow(tuple._2, 2))
+    map2.foreach((tuple: (String, Double)) => maginitudeB += pow(tuple._2, 2))
 
-    val maginitude: Double = sqrt(maginitudeA) + sqrt(maginitudeB)
+    val maginitude: Double = sqrt(maginitudeA) * sqrt(maginitudeB)
 
     val unionKey = map1.keySet.filter(map2.contains)
     var docProduct: Double = 0
-    unionKey.foreach((key: Any) => docProduct += map1(key) * map2(key))
+    unionKey.foreach((key: String) => docProduct += map1(key) * map2(key))
 
     docProduct / maginitude
   }
