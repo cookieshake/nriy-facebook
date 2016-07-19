7be27a91fa9a71affaa22ff8ec20d9c38c00dbb8
cookieshake <nriyays@gmail.com>
2016-07-20 03:43:22 +0900

    Fix compile error

--- src/main/scala/net/ingtra/nriyfacebook/tools/Timer.scala
+++ src/main/scala/net/ingtra/nriyfacebook/tools/Timer.scala
@@ -1,11 +1,11 @@
 package net.ingtra.nriyfacebook.tools
 
 object Timer {
-  var startTime: Long = null
+  var startTime: Long = -1
 
   def start(): Unit = startTime = System.nanoTime()
   def end(): Unit = {
-    if (startTime == null) println("Not started!")
+    if (startTime == -1) println("Not started!")
     else {
       val timeElapsed: Double = (System.nanoTime() - startTime).asInstanceOf[Double] / 1000000000
       println(s"Time Elapsed: $timeElapsed")
