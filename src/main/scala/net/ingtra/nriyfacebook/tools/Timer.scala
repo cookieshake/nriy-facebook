f5c2ba4887e5f2a044d01db7afe8faadb7c535a2
cookieshake <nriyays@gmail.com>
2016-07-20 03:41:51 +0900

    if not started?

--- src/main/scala/net/ingtra/nriyfacebook/tools/Timer.scala
+++ src/main/scala/net/ingtra/nriyfacebook/tools/Timer.scala
@@ -1,11 +1,14 @@
 package net.ingtra.nriyfacebook.tools
 
 object Timer {
-  var startTime: Long = 0
+  var startTime: Long = null
 
   def start(): Unit = startTime = System.nanoTime()
   def end(): Unit = {
-    val timeElapsed: Double = (System.nanoTime() - startTime).asInstanceOf[Double] / 1000000000
-    println(s"Time Elapsed: $timeElapsed")
+    if (startTime == null) println("Not started!")
+    else {
+      val timeElapsed: Double = (System.nanoTime() - startTime).asInstanceOf[Double] / 1000000000
+      println(s"Time Elapsed: $timeElapsed")
+    }
   }
 }
